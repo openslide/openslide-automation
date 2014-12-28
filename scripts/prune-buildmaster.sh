cef90cd8614553a961cd9ece838b83d18829aba6
Benjamin Gilbert <bgilbert@backtick.net>
2014-12-28 03:57:10 -0500

    Add script to garbage-collect buildmaster outputs

---
+++ scripts/prune-buildmaster.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+RETENTION=90  # days
+
+set -e
+
+if [ ! -e "$1/buildbot.tac" ] ; then
+    echo "Usage: $0 <buildbot-dir>"
+    exit 1
+fi
+
+# Delete old master logs
+find "$1" -maxdepth 1 \
+        -name "twistd.log*" -mtime +$RETENTION -delete
+
+# Delete old build logs
+find "$1" \
+        -name public_html -prune -o \
+        -name templates -prune -o \
+        -type f -regex "${1%/}/.*/[0-9].*" -mtime +$RETENTION -exec rm {} \;
+
+# Delete old build results
+find "${1%/}/public_html/results" \
+        -mtime +$RETENTION -delete -o \
+        -type d -empty -delete
+
+# winbuild snapshots are pruned by winbuild-index.py

f04d47bc7f1fc6290dbe0a403c567d808277ec5a
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2023-05-11 00:24:43 -0400

    Add nightly build script for s390x
    Signed-off-by: Benjamin Gilbert <bgilbert@cs.cmu.edu>

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-This repository contains fragments of old infrastructure for automatically
-building and testing OpenSlide.
+This repository contains infrastructure for automatically building and
+testing OpenSlide.
+
+- `autobuild`: automatic nightly build from Git main
+- `buildbot`: fragments of obsolete Buildbot configuration
---
+++ autobuild/test.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+# Copyright (c) 2023 Benjamin Gilbert
+# All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, version 2.1.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program. If not, see
+# <http://www.gnu.org/licenses/>.
+#
+
+set -euxo pipefail
+
+export OPENSLIDE_TEST_CACHE="/cache"
+SRC="/src"
+BUILD="/build"
+OUT="/out"
+
+# build
+cd "$SRC"
+git rev-parse HEAD
+if ! meson setup "$BUILD" --werror; then
+    cat "$BUILD/meson-logs/meson-log.txt"
+    exit 1
+fi
+cd "$BUILD"
+meson compile
+
+# smoke tests
+if ! meson test; then
+    cat meson-logs/testlog.txt
+    exit 1
+fi
+cat meson-logs/testlog.txt
+
+# unpack
+test/driver unfreeze
+test/driver unpack nonfrozen
+
+# test
+test/driver run
+test/driver valgrind
+test/driver mosaic "$OUT/mosaic.png"
